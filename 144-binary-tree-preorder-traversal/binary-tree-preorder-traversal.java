1ce3f469a4698f77d9030f2b42a6cd41c249818c
Pratik Mali <pratikmali271@gmail.com>
2026-07-15 12:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (85.61%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,15 +14,42 @@
  * }
  */
 class Solution {
-    List<Integer> list = new ArrayList<>();
+   // List<Integer> list = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root == null) {
-            return list;
-        }
-        list.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        // if(root == null) {
+        //     return list;
+        // }
+        // list.add(root.val);
+        // preorderTraversal(root.left);
+        // preorderTraversal(root.right);
 
+        // return list;
+
+        //Using Morris Traversal
+        List<Integer> list = new ArrayList<>();
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.left == null){
+                list.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right != null && prev.right != curr){
+                    prev = prev.right;
+                }
+                if(prev.right == null){
+                    prev.right = curr;
+                    list.add(curr.val);
+                    curr = curr.left;
+                } 
+                else{
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
         return list;
     }
 }
