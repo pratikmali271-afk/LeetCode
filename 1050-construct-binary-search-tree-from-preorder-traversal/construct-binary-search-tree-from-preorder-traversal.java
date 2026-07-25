b1013f486411350a8fbb99b77fc46e59846bbe19
Pratik Mali <pratikmali271@gmail.com>
2026-07-25 14:30:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.82%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -14,30 +14,40 @@
  * }
  */
 class Solution {
-    Map<Integer, Integer> map = new HashMap<>();
+    // Map<Integer, Integer> map = new HashMap<>();
     int preindex = 0;
 
-    public TreeNode buildTree(int[] pre, int[] in){
-        for(int i = 0; i < pre.length; i++){
-            map.put(in[i], i);
-        }
+    // public TreeNode buildTree(int[] pre, int[] in){
+    //     for(int i = 0; i < pre.length; i++){
+    //         map.put(in[i], i);
+    //     }
 
-        return build(pre, 0, in.length - 1);
-    }
-    public TreeNode build(int[] pre, int inStart, int inEnd){
-        if(inStart > inEnd) return null;
+    //     return build(pre, 0, in.length - 1);
+    // }
+    // public TreeNode build(int[] pre, int inStart, int inEnd){
+    //     if(inStart > inEnd) return null;
+
+    //     TreeNode root = new TreeNode(pre[preindex++]);
+    //     int index = map.get(root.val);
+
+    //     root.left = build(pre, inStart, index - 1);
+    //     root.right = build(pre, index + 1, inEnd);
+
+    //     return root;
+    // }
+    public TreeNode buildBST(int[] pre, int bound){
+        if(preindex == pre.length || pre[preindex] > bound) return null;
 
         TreeNode root = new TreeNode(pre[preindex++]);
-        int index = map.get(root.val);
-
-        root.left = build(pre, inStart, index - 1);
-        root.right = build(pre, index + 1, inEnd);
+        root.left = buildBST(pre, root.val);
+        root.right = buildBST(pre, bound);
 
         return root;
     }
     public TreeNode bstFromPreorder(int[] preorder) {
-        int[] inorder = preorder.clone();
-        Arrays.sort(inorder);
-        return buildTree(preorder, inorder);
+        // int[] inorder = preorder.clone();
+        // Arrays.sort(inorder);
+        // return buildTree(preorder, inorder);
+        return buildBST(preorder, Integer.MAX_VALUE);
     }
 }
