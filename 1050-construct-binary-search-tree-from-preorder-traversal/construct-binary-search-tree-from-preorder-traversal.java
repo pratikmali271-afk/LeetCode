02d4367240f89e483b3f6dce5ea96fb8277b5cd4
Pratik Mali <pratikmali271@gmail.com>
2026-07-25 13:44:23 +0530

    Time: 4 ms (6.92%) | Memory: 43.8 MB (5.14%) - LeetSync

---
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    int preindex = 0;
+
+    public TreeNode buildTree(int[] pre, int[] in){
+        for(int i = 0; i < pre.length; i++){
+            map.put(in[i], i);
+        }
+
+        return build(pre, 0, in.length - 1);
+    }
+    public TreeNode build(int[] pre, int inStart, int inEnd){
+        if(inStart > inEnd) return null;
+
+        TreeNode root = new TreeNode(pre[preindex++]);
+        int index = map.get(root.val);
+
+        root.left = build(pre, inStart, index - 1);
+        root.right = build(pre, index + 1, inEnd);
+
+        return root;
+    }
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int[] inorder = preorder.clone();
+        Arrays.sort(inorder);
+        return buildTree(preorder, inorder);
+    }
+}
