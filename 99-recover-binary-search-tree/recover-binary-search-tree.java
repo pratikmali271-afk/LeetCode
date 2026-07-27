812d9aed7feeb81f4f2d736cf6e9224bc47aec29
Pratik Mali <pratikmali271@gmail.com>
2026-07-27 12:10:09 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (87.43%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -13,27 +13,60 @@
  *     }
  * }
  */
+
+ // Method 1
+// class Solution {
+
+//     List<TreeNode> nodes = new ArrayList<>();
+//     List<Integer> values = new ArrayList<>();
+
+//     public void inorder(TreeNode root) {
+//         if (root == null) return;
+
+//         inorder(root.left);
+//         nodes.add(root);
+//         values.add(root.val);
+//         inorder(root.right);
+//     }
+
+//     public void recoverTree(TreeNode root) {
+//         inorder(root);
+
+//         Collections.sort(values);
+
+//         for (int i = 0; i < nodes.size(); i++) {
+//             nodes.get(i).val = values.get(i);
+//         }
+//     }
+// }
+
+//Method 2 expected
 class Solution {
+    private TreeNode first;
+    private TreeNode last;
+    private TreeNode prev;
 
-    List<TreeNode> nodes = new ArrayList<>();
-    List<Integer> values = new ArrayList<>();
-
-    public void inorder(TreeNode root) {
-        if (root == null) return;
-
+    public void inorder(TreeNode root){
+        if(root == null) return;
+        
         inorder(root.left);
-        nodes.add(root);
-        values.add(root.val);
+
+        if(prev != null && root.val < prev.val){
+            if(first == null) first = prev;
+            last = root;
+        }
+        prev = root;
+
         inorder(root.right);
     }
-
-    public void recoverTree(TreeNode root) {
+    public void recoverTree(TreeNode root){
+        first = last = prev = null;
         inorder(root);
 
-        Collections.sort(values);
-
-        for (int i = 0; i < nodes.size(); i++) {
-            nodes.get(i).val = values.get(i);
+        if(first != null && last != null){
+            int t = first.val;
+            first.val = last.val;
+            last.val = t;
         }
     }
 }
