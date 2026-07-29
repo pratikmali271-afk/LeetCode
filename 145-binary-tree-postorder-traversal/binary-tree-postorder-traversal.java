6267a8bb293084f6b6f15d8c12855767a44d7e3f
Pratik Mali <pratikmali271@gmail.com>
2026-07-29 22:00:26 +0530

    Time: 1 ms (11.79%) | Memory: 43.2 MB (33.36%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -28,26 +28,61 @@ class Solution {
     // }
 
     //Using 2 stack
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> list = new ArrayList<>();
+        
+    //     if(root == null) return new ArrayList<>();
+
+    //     Stack<TreeNode> st1 = new Stack<>();
+    //     Stack<TreeNode> st2 = new Stack<>();
+
+    //     st1.push(root);
+    //     while(!st1.isEmpty()){
+    //         root = st1.pop();
+    //         st2.push(root);
+
+    //         if(root.left != null) st1.push(root.left);
+    //         if(root.right != null) st1.push(root.right);
+    //     }
+
+    //     while(!st2.isEmpty()){
+    //         list.add(st2.pop().val);
+    //     }
+    //     return list;
+    // }
+
+    //Using 1 stack
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        
-        if(root == null) return new ArrayList<>();
 
-        Stack<TreeNode> st1 = new Stack<>();
-        Stack<TreeNode> st2 = new Stack<>();
+        if(root == null)
+            return list;
 
-        st1.push(root);
-        while(!st1.isEmpty()){
-            root = st1.pop();
-            st2.push(root);
+        Stack<TreeNode> st = new Stack<>();
 
-            if(root.left != null) st1.push(root.left);
-            if(root.right != null) st1.push(root.right);
+        TreeNode curr = root;
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                TreeNode temp = st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    list.add(temp.val);
+
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.pop();
+                        list.add(temp.val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
         }
 
-        while(!st2.isEmpty()){
-            list.add(st2.pop().val);
-        }
         return list;
     }
 }
