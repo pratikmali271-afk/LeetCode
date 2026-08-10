2e348d05b087ea6f6b36e67430140f5792a40bfc
Pratik Mali <pratikmali271@gmail.com>
2026-08-10 18:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (38.52%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,28 +1,61 @@
+// Using BFS
+// class Solution {
+//     public boolean isBipartite(int[][] graph) {
+//         int n = graph.length;
+//         int[] color = new int[n];
+
+//         for(int i = 0; i < n; i++){
+//             color[i] = -1;
+//         }
+
+//         for(int i = 0; i < n; i++){
+//             if(color[i] != -1) continue;
+
+//             Queue<Integer> q = new LinkedList<>();
+//             q.offer(i);
+//             color[i] = 0;
+
+//             while(!q.isEmpty()){
+//                 int curr = q.poll();
+
+//                 for(int neigh : graph[curr]){
+//                     if(color[neigh] == -1){
+//                         color[neigh] = 1 - color[curr];
+//                         q.offer(neigh);
+//                     } else if(color[neigh] == color[curr]) return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+// Using DFS
 class Solution {
+    public boolean dfs(int node, int[][] graph, int[] color){
+        for(int neigh : graph[node]){
+            if(color[neigh] == -1){
+                color[neigh] = 1 - color[node];
+
+                if(!dfs(neigh, graph, color)) return false;
+            }
+            else if(color[neigh] == color[node]) return false;
+        }
+
+        return true;
+    }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         int[] color = new int[n];
 
-        for(int i = 0; i < n; i++){
-            color[i] = -1;
-        }
+        for(int i = 0; i < n; i++) color[i] = -1;
 
         for(int i = 0; i < n; i++){
             if(color[i] != -1) continue;
-
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(i);
             color[i] = 0;
-
-            while(!q.isEmpty()){
-                int curr = q.poll();
-
-                for(int neigh : graph[curr]){
-                    if(color[neigh] == -1){
-                        color[neigh] = 1 - color[curr];
-                        q.offer(neigh);
-                    } else if(color[neigh] == color[curr]) return false;
-                }
+            
+            if (!dfs(i, graph, color)){
+                return false;
             }
         }
         return true;
