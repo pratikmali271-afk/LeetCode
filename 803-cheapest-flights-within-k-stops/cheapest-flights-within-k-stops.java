0fb3c4eb0a60572caa31e4130e34aaf13f24eea5
Pratik Mali <pratikmali271@gmail.com>
2026-09-06 11:13:10 +0530

    Time: 5 ms (98.82%) | Memory: 46.8 MB (26.39%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,58 +1,86 @@
-class pair{
-    int first;
-    int second;
-    pair(int first, int second){
-        this.first = first;
-        this.second = second;
-    }
-}
-class tuple{
-    int stops;
-    int node;
-    int distance;
-    tuple(int stops, int node, int distance){
-        this.stops = stops;
-        this.node = node;
-        this.distance = distance;
-    }
-}
+// class pair{
+//     int first;
+//     int second;
+//     pair(int first, int second){
+//         this.first = first;
+//         this.second = second;
+//     }
+// }
+// class tuple{
+//     int stops;
+//     int node;
+//     int distance;
+//     tuple(int stops, int node, int distance){
+//         this.stops = stops;
+//         this.node = node;
+//         this.distance = distance;
+//     }
+// }
+// class Solution {
+//     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+//         List<List<pair>> adj = new ArrayList<>();
+//         for(int i = 0; i < n; i++){
+//             adj.add(new ArrayList<>());
+//         }
+        
+//         for (int i = 0; i < flights.length; i++) {
+//             adj.get(flights[i][0]).add(new pair(flights[i][1], flights[i][2]));
+//         }
+
+//         Queue<tuple> q = new LinkedList<>();
+//         q.offer(new tuple(0, src, 0));
+
+//         int[] dist = new int[n];
+//         Arrays.fill(dist, Integer.MAX_VALUE);
+//         dist[src] = 0;
+
+//         while(!q.isEmpty()){
+//             tuple curr = q.poll();
+//             int stops = curr.stops;
+//             int node = curr.node;
+//             int cost = curr.distance;
+
+//             if(stops > k) continue;
+
+//             for(pair it : adj.get(node)){
+//                 int adjNode = it.first;
+//                 int adjdist = it.second;
+
+//                 if(cost + adjdist < dist[adjNode] && stops <= k){
+//                     dist[adjNode] = cost + adjdist;
+//                     q.offer(new tuple(stops + 1, adjNode, cost + adjdist));
+//                 }
+//             }
+//         }
+//         if(dist[dst] == Integer.MAX_VALUE) return -1;
+//         return dist[dst];
+//     }
+// }
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<pair>> adj = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < flights.length; i++) {
-            adj.get(flights[i][0]).add(new pair(flights[i][1], flights[i][2]));
-        }
-
-        Queue<tuple> q = new LinkedList<>();
-        q.offer(new tuple(0, src, 0));
-
         int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
 
-        while(!q.isEmpty()){
-            tuple curr = q.poll();
-            int stops = curr.stops;
-            int node = curr.node;
-            int cost = curr.distance;
+        // At most k stops = k + 1 edges
+        for (int i = 0; i < k + 1; i++) {
+            int[] temp = dist.clone();
 
-            if(stops > k) continue;
+            for (int[] edge : flights) {
+                int u = edge[0];
+                int v = edge[1];
+                int price = edge[2];
 
-            for(pair it : adj.get(node)){
-                int adjNode = it.first;
-                int adjdist = it.second;
-
-                if(cost + adjdist < dist[adjNode] && stops <= k){
-                    dist[adjNode] = cost + adjdist;
-                    q.offer(new tuple(stops + 1, adjNode, cost + adjdist));
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + price < temp[v]) {
+                    temp[v] = dist[u] + price;
                 }
             }
+            dist = temp;
         }
-        if(dist[dst] == Integer.MAX_VALUE) return -1;
+        if (dist[dst] == Integer.MAX_VALUE) {
+            return -1;
+        }
         return dist[dst];
     }
 }
