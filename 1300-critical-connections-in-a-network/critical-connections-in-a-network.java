ce8fafd72d90fe7e83e8dc8c858a730bc4e89a12
Pratik Mali <pratikmali271@gmail.com>
2026-09-17 18:33:57 +0530

    Time: 90 ms (57.78%) | Memory: 207.9 MB (75.16%) - LeetSync

---
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -0,0 +1,43 @@
+class Solution {
+    private int timer = 1;
+    private void dfs(int node, int parent, boolean[] vis, int[] tin, int[] low, List<List<Integer>> bridges,  List<List<Integer>> adj){
+        vis[node] = true;
+        tin[node] = low[node] = timer;
+        timer++;
+
+        for(Integer it : adj.get(node)){
+            if(it == parent) continue;
+            if(!vis[it]){
+                dfs(it, node, vis, tin, low, bridges, adj);
+                low[node] = Math.min(low[node], low[it]);
+
+                if(low[it] > tin[node]){
+                    bridges.add(Arrays.asList(it, node));
+                }
+            }
+            else{
+                low[node] = Math.min(low[node], low[it]);
+            }
+        }
+    }
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(List<Integer> it : connections){
+            int u = it.get(0);
+            int v = it.get(1);
+
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        boolean[] vis = new boolean[n];
+        int[] tin = new int[n];
+        int[] low = new int[n];
+        List<List<Integer>> bridges = new ArrayList<>();
+        dfs(0, -1, vis, tin, low, bridges, adj);
+        return bridges;
+    }
+}
