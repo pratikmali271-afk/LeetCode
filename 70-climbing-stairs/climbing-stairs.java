1cc27ad1d47fc025d1a522b3a1069d0362e4ecd5
Pratik Mali <pratikmali271@gmail.com>
2026-09-20 14:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (36.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,3 +1,4 @@
+// Recursion
 // class Solution {
 //     public int climbStairs(int n) {
 //         if(n == 0 || n == 1) return 1;
@@ -7,18 +8,34 @@
 //     }
 // }
 
+// // Menoization
+// class Solution {
+//     int[] dp;
+//     public int climbStairs(int n) {
+//         dp = new int[n + 1];
+//         return solve(n);
+//     }
+
+//     public int solve(int n) {
+//         if(n <= 1) return 1;
+//         if(dp[n] != 0) return dp[n];
+
+//         dp[n] = solve(n - 1) + solve(n - 2);
+
+//         return dp[n];
+//     }
+// }
+
+// tabulation
 class Solution {
-    int[] dp;
     public int climbStairs(int n) {
-        dp = new int[n + 1];
-        return solve(n);
-    }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-    public int solve(int n) {
-        if(n <= 1) return 1;
-        if(dp[n] != 0) return dp[n];
-
-        dp[n] = solve(n - 1) + solve(n - 2);
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
         return dp[n];
     }
