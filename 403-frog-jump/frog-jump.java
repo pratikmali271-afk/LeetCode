e1d89bcf9652383a02ad838fdb2b8c294f1b04c7
Pratik Mali <pratikmali271@gmail.com>
2026-09-22 10:38:40 +0530

    Time: 90 ms (13.36%) | Memory: 184.7 MB (21.85%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,38 +1,36 @@
 class Solution {
     public boolean canCross(int[] stones) {
         int n = stones.length;
-        if(stones[1] != 1) return false;
-        Boolean[][] dp = new Boolean[n][n + 1];
 
         // First jump must be 1
+        if(stones[1] != 1) return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            map.put(stones[i], i);
+        }
+
+        Boolean[][] dp = new Boolean[n][n + 1];
         dp[1][1] = true;
+
         for(int i = 1; i < n; i++) {
             for(int lastJump = 1; lastJump <= n; lastJump++) {
-                // Frog cannot reach this state
-                if(dp[i][lastJump] == null || !dp[i][lastJump]) continue;
+                if(dp[i][lastJump] == null) continue;
 
-                // Try lastJump - 1, lastJump, lastJump + 1
                 for(int jump = lastJump - 1; jump <= lastJump + 1; jump++) {
                     if(jump <= 0) continue;
 
                     int nextPosition = stones[i] + jump;
-                    for(int idx = i + 1; idx < n; idx++) {
-                        if(stones[idx] == nextPosition) {
-                            dp[idx][jump] = true;
-                            break;
-                        }
-
-                        if(stones[idx] > nextPosition) break;
+                    if(map.containsKey(nextPosition)) {
+                        int nextIndex = map.get(nextPosition);
+                        dp[nextIndex][jump] = true;
                     }
                 }
             }
         }
 
-        // Did we reach the last stone?
         for(int jump = 1; jump <= n; jump++) {
-            if(dp[n - 1][jump] != null && dp[n - 1][jump]) {
-                return true;
-            }
+            if(dp[n - 1][jump] != null) return true;
         }
         return false;
     }
